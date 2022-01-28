6db8a44a3bb1b30a6f1da549344622961e1a1c0e
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-28 22:16:40 +0900

    [Add] 프로젝트 파일 생성

---
+++ MyPhotos/MyPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPhotos
+//
+//  Created by 정은희 on 2022/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
