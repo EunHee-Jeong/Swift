0691bd5641601842fc97f73b4380d1879cbabfef
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-02-01 20:50:30 +0900

    [Feat] 웹에서 이미지 받아오기
    - Async → 동기 → 작업이 끝나기 전까지 다음으로 넘어가지 않는 문제점

--- MyPhotos/MyPhotos/Presentation/ViewControllers/AsyncViewController.swift
+++ MyPhotos/MyPhotos/Presentation/ViewControllers/AsyncViewController.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
-class AsyncViewController: UIViewController {
-
+final class AsyncViewController: UIViewController {
+    
+    // MARK: - @IBOutlet Part
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    // MARK: - @IBAction Part
+    @IBAction func touchUpDownloadButton(_ sender: UIButton) {
+        guard let imageURL: URL = URL(string: "https://img.redbull.com/images/c_fill,g_auto,w_1500,h_960/q_auto,f_auto/redbullcom/2016/09/20/1331818966444_2/pok%C3%A9mon-super-mystery-dungeon") else { return }
+        do {
+            let imageData: Data = try Data.init(contentsOf: imageURL)   // ✅ async 부분
+            guard let image: UIImage = UIImage(data: imageData) else { return }
+            self.imageView.image = image
+        } catch { return }
+        
     }
 }
