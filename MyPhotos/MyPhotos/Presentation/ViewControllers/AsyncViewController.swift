b4a30bd105ea50e2e105a20902523c03519e68b3
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-02-01 20:17:23 +0900

    [Add] 비동기 프로그래밍 연습용 뷰, 컨트롤러 추가

---
+++ MyPhotos/MyPhotos/Presentation/ViewControllers/AsyncViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AsyncViewController.swift
+//  MyPhotos
+//
+//  Created by 정은희 on 2022/02/01.
+//
+
+import UIKit
+
+class AsyncViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
