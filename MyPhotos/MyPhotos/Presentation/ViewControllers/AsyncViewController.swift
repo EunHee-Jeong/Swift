1d298bac334004504d2c833f8087a737039c5780
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-02-01 20:55:04 +0900

    [Edit] 작동 방식 변경
    - OperationQueue().addOperation(_:) 이용
    - 메인 스레드가 아닌 백그라운드(=다른 스레드)에서 동작하게 해서 해결

--- MyPhotos/MyPhotos/Presentation/ViewControllers/AsyncViewController.swift
+++ MyPhotos/MyPhotos/Presentation/ViewControllers/AsyncViewController.swift
@@ -21,11 +21,14 @@ final class AsyncViewController: UIViewController {
     // MARK: - @IBAction Part
     @IBAction func touchUpDownloadButton(_ sender: UIButton) {
         guard let imageURL: URL = URL(string: "https://img.redbull.com/images/c_fill,g_auto,w_1500,h_960/q_auto,f_auto/redbullcom/2016/09/20/1331818966444_2/pok%C3%A9mon-super-mystery-dungeon") else { return }
-        do {
-            let imageData: Data = try Data.init(contentsOf: imageURL)   // ✅ async 부분
-            guard let image: UIImage = UIImage(data: imageData) else { return }
-            self.imageView.image = image
-        } catch { return }
-        
+        OperationQueue().addOperation {
+            do {
+                let imageData: Data = try Data.init(contentsOf: imageURL)   // async 부분
+                guard let image: UIImage = UIImage(data: imageData) else { return }
+                OperationQueue.main.addOperation {
+                    self.imageView.image = image    // UI와 관련된 코드는 메인 스레드에서 동작하도록
+                }
+            } catch { return }
+        }
     }
 }
