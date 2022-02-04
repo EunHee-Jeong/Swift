0758b95af208b434195311bc3c956ef97e8a94f1
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-02-04 10:21:03 +0900

    [Feat] 코드로 뷰 구현
    - 객체 선언 → 관계 형성 → 속성 추가 → 동작 지정
    - 동작 지정: 함수 만들고(sender), 연결 (addTarget)

--- UI/UI/Presentation/Controllers/ViewController.swift
+++ UI/UI/Presentation/Controllers/ViewController.swift
@@ -1,29 +1,109 @@
 //
 //  ViewController.swift
-//  UI
+//  CodeBaseAutoLayout
 //
 //  Created by 정은희 on 2022/02/04.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
 
+final class ViewController: BaseViewController {
+    
+    // MARK: - Properties
+    private var redButton: UIButton = UIButton()
+    private var blueButton: UIButton = UIButton()
+    private var greenButton: UIButton = UIButton()
+    private var pinkButton: UIButton = UIButton()
+    private var resultLabel: UILabel = UILabel()
+
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+    // MARK: - Functions
+    override func layout() {
+        
+        [redButton, blueButton, greenButton, pinkButton].forEach {
+            view.addSubview($0)
+        }
+        view.addSubview(resultLabel)
+        
+        redButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        blueButton.snp.makeConstraints { make in
+            // top, leading 은 + 값을 줘야 벌어지고,
+            // bottom, trailing은 - 값을 줘야 벌어진다.
+            make.top.equalTo(redButton.snp.bottom).offset(10)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        greenButton.snp.makeConstraints { make in
+            make.top.equalTo(blueButton.snp.bottom).offset(10)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        pinkButton.snp.makeConstraints { make in
+            make.top.equalTo(greenButton.snp.bottom).offset(10)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        resultLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-80)
+        }
+        
+        [redButton, blueButton, greenButton, pinkButton].forEach {
+            $0.addTarget(self, action: #selector(onClickButton(_:)), for: .touchUpInside)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func style() {
+        [redButton, blueButton, greenButton, pinkButton].forEach {
+            $0.setTitleColor(.white, for: .normal)
+        }
+        redButton.backgroundColor = .systemRed
+        redButton.setTitle("Red Button", for: .normal)
+        blueButton.backgroundColor = .systemBlue
+        blueButton.setTitle("Blue Button", for: .normal)
+        greenButton.backgroundColor = .systemGreen
+        greenButton.setTitle("Green Button", for: .normal)
+        pinkButton.backgroundColor = .systemPink
+        pinkButton.setTitle("Pink Button", for: .normal)
+        resultLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        resultLabel.textAlignment = .center
+        resultLabel.text = "버튼을 클릭하세요"
     }
-    */
-
+    
+    @objc
+    func onClickButton(_ sender: UIButton) {
+        switch sender {
+        case redButton:
+            resultLabel.text = "Red Button Clicked."
+        case blueButton:
+            resultLabel.text = "Blue Button Clicked."
+        case greenButton:
+            resultLabel.text = "Green Button Clicked."
+        case pinkButton:
+            resultLabel.text = "Pink Button Clicked."
+        default:
+            break
+        }
+    }
 }
+
