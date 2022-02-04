18d853b13b01f0be165872456af448e7b4d10e74
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-02-04 10:03:12 +0900

    [Set] 폴더링 수정과 파일 삭제

---
+++ UI/UI/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  UI
+//
+//  Created by 정은희 on 2022/01/04.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)    // 기본 화면 설정
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController(nibName: nil, bundle: nil)
+        window?.makeKeyAndVisible()
+        
+    }
+}
+
