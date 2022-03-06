f0b500294cadf33180e4c6289e3f5cb439f34d45
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-03-07 00:10:11 +0900

    [Set] FCM 설정

--- Notification/Notification/Application/AppDelegate.swift
+++ Notification/Notification/Application/AppDelegate.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+import Firebase
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UNUserNotificationCenter.current().delegate = self
+        FirebaseApp.configure() // ✅ Firebase 초기화
         return true
     }
 
