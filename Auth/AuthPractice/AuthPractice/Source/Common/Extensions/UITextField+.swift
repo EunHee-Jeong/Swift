6e374672e8eb6ebc6174631f71638cfba1c8aa75
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-04-18 22:47:49 +0900

    [Style] UI 구현
    - 경고창 표시 → 스택 뷰 동적으로 구성
    - 버튼 디폴트 → Enable 체크 해제

---
+++ Auth/AuthPractice/AuthPractice/Source/Common/Extensions/UITextField+.swift
@@ -0,0 +1,21 @@
+//
+//  UITextField+.swift
+//  AuthPractice
+//
+//  Created by 정은희 on 2022/04/18.
+//
+
+import UIKit
+
+extension UITextField {
+    public func setLeftPadding(amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    public func setRightPadding(amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
--- Auth/AuthPractice/AuthPractice/Source/Presentation/SignInViewController.swift
+++ Auth/AuthPractice/AuthPractice/Source/Presentation/SignInViewController.swift
@@ -7,9 +7,25 @@
 
 import UIKit
 
-class SignInViewController: BaseViewController {
-
+final class SignInViewController: BaseViewController {
+    
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func style() {
+        super.style()
+        
+        [emailTextField, passwordTextField, checkTextField].forEach {
+            $0?.font = .systemFont(ofSize: 18)
+            $0?.setLeftPadding(amount: 10)
+        }
+        nextButton.layer.cornerRadius = 12
     }
 }
