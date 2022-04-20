dfa95668d73589be8f8d37e6b111ba402304ee44
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-04-20 21:14:36 +0900

    [Add] 프로젝트 파일 생성과 에셋 추가

---
+++ AR/FloorIsLava/FloorIsLava/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloorIsLava
+//
+//  Created by 정은희 on 2022/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
