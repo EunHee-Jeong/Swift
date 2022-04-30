816239d2e7926e57bbc17304bde6f9170405112e
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-04-30 14:07:48 +0900

    [Edit] BaseVC 코드 수정
    - 네이밍
    - 접근 방식 변경 (public → internal)

--- UI/UI/Presentation/Common/BaseViewController.swift
+++ UI/UI/Presentation/Common/BaseViewController.swift
@@ -9,27 +9,19 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        style()
+        setHierarchy()
         setNavigation()
-        hierarchy()
-        layout()
+        setTargets()
+        setStyle()
     }
 
-    public func style() { }
-    public func setNavigation(backgroundColor: UIColor = .clear) {
-        guard let navigationBar = navigationController?.navigationBar else { return }
-        navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationBar.shadowImage = UIImage()
-        
-        let appearance = UINavigationBarAppearance()
-        appearance.backgroundColor = backgroundColor
-        
-        guard let navigationController = navigationController else { return }
-        navigationController.hidesBarsOnSwipe = true
-    }
-    public func hierarchy() { }
-    public func layout() { }
+    // MARK: - Functions
+    func setHierarchy() { }
+    func setNavigation() { }
+    func setTargets() { }
+    func setStyle() { }
 }
